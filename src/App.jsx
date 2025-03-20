489adb939a000fe6d779c930c6499ee9894e4a00
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2025-03-20 18:37:09 -0300

    initialize React app with Vite and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-gray-500">
+        {/* <Navbar />
+        <Hero /> */}
+        <p>Estoy aquí</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
